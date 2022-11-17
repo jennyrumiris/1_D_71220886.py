8ad8f1eae6d3b304d0493df5850347d815d10ee0
jennyrumiris <117902222+jennyrumiris@users.noreply.github.com>
2022-11-17 16:27:52 +0700

    Add files via upload

---
+++ 3_D_71220886.py
@@ -0,0 +1,8 @@
+name=input("masukan nama anda:")
+print(name)
+matkul=input("masukkan mata kuliah:")
+print(matkul)
+grup=("masukkan grup:")
+print(grup)
+
+print("Berikut ini merupakan contoh program penjumlahan")
